679550bdc0e3315a95a47c148a57a890515d828c
cjmurphy93 <cjmurph93@gmail.com>
2021-05-17 23:44:05 -0400

    starting app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue[900],
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text('Ask Me Anything!'),
+          backgroundColor: Colors.blue[600],
+        ),
+        body: EightBall(),
       ),
-    );
+    ),
+  );
+}
